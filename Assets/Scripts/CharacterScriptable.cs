d6396d1cce77ee9d711397abda1adb0bb7a5aef0
Samuel Busson <samuel.busson@hotmail.fr>
2020-02-01 12:23:19 +0100

    Add generateScriptableObject

--- Assets/Scripts/CharacterScriptable.cs
+++ Assets/Scripts/CharacterScriptable.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
 public class CharacterScriptable : ScriptableObject
@@ -9,16 +10,39 @@ public class CharacterScriptable : ScriptableObject
    [SerializeField]
    Character character;
 
-    public void Init(string c_Name, string c_Surname, bool nameRandom, int[] gearValue, Vector2[] gearExpectation, int hero, bool privateText, string forcedText)
+    public void Init(string c_Name, string c_Surname, bool nameRandom, int[] gearValue, Vector2[] gearExpectation, float hero, bool privateText, string forcedText, int ironAdd)
     {
-        character.c_Name = c_Name;
+        character = new Character(c_Name, c_Surname, nameRandom, gearValue, gearExpectation, hero, privateText, forcedText, ironAdd);
+    /*    character.c_Name = c_Name;
         character.c_Surname = c_Surname;
         character.nameRandom = nameRandom;
         character.gearValue = gearValue;
         character.gearExpectation = gearExpectation;
         character.hero = hero;
         character.privateText = privateText;
-        character.forcedText = forcedText;
+        character.forcedText = forcedText;*/
+    }
+
+    public static CharacterScriptable CreateInstance(string c_Name, string c_Surname, bool nameRandom, int[] gearValue, Vector2[] gearExpectation, float hero, bool privateText, string forcedText, int ironAdd)
+    {
+        CharacterScriptable data = CreateInstance<CharacterScriptable>();
+        data.Init(c_Name, c_Surname, nameRandom, gearValue, gearExpectation, hero, privateText, forcedText, ironAdd);
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+        path = "Assets/Data/Scriptable/o_";
+
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + c_Name + " " + c_Surname + ".asset");
+
+        AssetDatabase.CreateAsset(data, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = data;
+
+
+        return data;
     }
 
 
