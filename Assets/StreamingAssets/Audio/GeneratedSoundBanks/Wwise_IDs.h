c0c75b6e3db96b62844c71cf41e4e194f84d1f23
Guillaume <red.tri@outlook.com>
2020-02-02 12:06:00 +0100

    56321

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,47 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_BATTLEFIELDAMBIANCE = 3316639316U;
         static const AkUniqueID PLAY_TEST = 3187507146U;
+        static const AkUniqueID SET_DOORCLOSE = 238286794U;
+        static const AkUniqueID SET_DOOROPEN = 2718873070U;
+        static const AkUniqueID SET_INTENSITYCALM = 4236667796U;
+        static const AkUniqueID SET_INTENSITYEXTREME = 1912983523U;
+        static const AkUniqueID SET_INTENSITYHIGH = 4034190369U;
+        static const AkUniqueID SET_INTENSITYLOW = 3635806469U;
+        static const AkUniqueID SET_INTENSITYMEDIUM = 716215376U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace DOORSTATES
+        {
+            static const AkUniqueID GROUP = 33159115U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CLOSED = 3012222945U;
+                static const AkUniqueID OPEN = 3072142513U;
+            } // namespace STATE
+        } // namespace DOORSTATES
+
+        namespace WINNINGSTATE
+        {
+            static const AkUniqueID GROUP = 125032842U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LOOSING = 1545774708U;
+                static const AkUniqueID WINNING = 2996998095U;
+            } // namespace STATE
+        } // namespace WINNINGSTATE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID INTENSITY = 2470328564U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -25,6 +64,7 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMBIANCES = 1404066300U;
+        static const AkUniqueID BATTLEFIELD_DRY = 1210761507U;
         static const AkUniqueID FOLEYS = 4035004657U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID SFX = 393239870U;
@@ -34,6 +74,7 @@ namespace AK
 
     namespace AUX_BUSSES
     {
+        static const AkUniqueID BATTLEFIELD_FILTERED = 1550058001U;
         static const AkUniqueID RVB_BATTLEFIELD = 1775981102U;
         static const AkUniqueID RVB_FORGE = 3907231393U;
     } // namespace AUX_BUSSES
